0ecf7a8552194e58d723aafc40f8c687f17a0b4d
Meyer Charlotte Irma <cimeyer@INTRANET.EPFL.CH@SV-UniUbuntu43.intranet.epfl.ch>
2018-11-05 16:29:43 +0100

    changements dans random_connect

--- NetSim/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cimeyer/Desktop/NetSim
-# Build directory: /home/cimeyer/Desktop/NetSim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Network_simulation "testNetSim")
